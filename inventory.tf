833d4fccbe697cee516662bd71168da7ac10811c
Daniel Del Ciancio <dciancio@gmail.com>
2019-02-07 12:19:07 -0500

    aws fixes

--- inventory.tf
+++ inventory.tf
@@ -6,6 +6,14 @@ locals {
   infra_node_labels   = "openshift_node_labels=\"{'region': 'infra', 'zone': 'default'}\""
   compute_node_labels = "openshift_schedulable=true openshift_node_labels=\"{'region': 'primary', 'zone': 'default'}\""
 }
+data "template_file" "aws_config" {
+  count = "${var.cloudprovider == "aws" ? 1 : 0 }"
+  template = "${file("${path.cwd}/helper_scripts/aws_config.template")}"
+  vars {
+    cloudprovider = "${var.cloudprovider}"
+    clusterid = "${var.clustername}"
+  }
+}
 data "template_file" "masters" {
   count = "${var.master_count}"
   template = "${file("${path.cwd}/helper_scripts/masters.template")}"
@@ -40,8 +48,7 @@ data "template_file" "nodes_worker" {
 data "template_file" "inventory" {
   template = "${file("${path.cwd}/helper_scripts/ansible-hosts.template")}"
   vars {
-    cloudprovider = "${var.cloudprovider}"
-    clusterid = "${var.clustername}"
+    aws_config = "${join("",data.template_file.aws_config.*.rendered)}"
     ocp_version = "${var.ocp_version}"
     sdn_type = "${var.sdn_type}"
     public_subdomain = "${local.public_subdomain}"
