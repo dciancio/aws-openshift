2795020a6f8d10b542001c9bc03f10238bec9f42
Daniel Del Ciancio <dciancio@gmail.com>
2019-03-13 16:06:12 -0400

    sysprep fixes

--- helper_scripts/sysprep-bastion.sh
+++ helper_scripts/sysprep-bastion.sh
@@ -3,27 +3,27 @@
 exec >/var/log/cloud-init-output.log 2>&1
 
 HN=$(curl http://169.254.169.254/latest/meta-data/hostname)
-sudo hostnamectl set-hostname $${HN}.${ec2domain}
+hostnamectl set-hostname $${HN}.${ec2domain}
 
-sudo rpm -e rh-amazon-rhui-client
-sudo yum clean all
-sudo rm -rf /var/cache/yum
+rpm -e rh-amazon-rhui-client
+yum clean all
+rm -rf /var/cache/yum
 
-sudo subscription-manager register --activationkey='${rhak}' --org='${rhorg}' --force
-sudo subscription-manager status
-sudo subscription-manager repos --disable="*"
-sudo subscription-manager repos \
+subscription-manager register --activationkey='${rhak}' --org='${rhorg}' --force
+subscription-manager status
+subscription-manager repos --disable="*"
+subscription-manager repos \
     --enable="rhel-7-server-rpms" \
     --enable="rhel-7-server-extras-rpms" \
     --enable="rhel-7-fast-datapath-rpms" \
     --enable="rhel-7-server-ose-${ocp_version}-rpms" \
     --enable="rhel-7-server-ansible-${ansible_version}-rpms"
 
-sudo yum update -y
-sudo yum install -y openshift-ansible
-sudo yum install -y wget git net-tools bind-utils yum-utils iptables-services bridge-utils bash-completion kexec-tools sos psacct unzip
+yum update -y
+yum install -y openshift-ansible
+yum install -y wget git net-tools bind-utils yum-utils iptables-services bridge-utils bash-completion kexec-tools sos psacct unzip
 
-sudo su - ec2-user bash -c 'cat <<EOF > ~/ansible.cfg
+su - ec2-user bash -c "cat >~/ansible.cfg <<EOF
 [defaults]
 log_path = ./ansible.log
 host_key_checking = False
@@ -34,14 +34,14 @@ roles_path = roles
 [ssh_connection]
 ssh_args = -C -o ControlMaster=auto -o ControlPersist=900s -o GSSAPIAuthentication=no
 pipelining = True
-EOF'
+EOF"
 
-sudo su - ec2-user bash -c 'ln -s /usr/share/ansible/openshift-ansible'
+su - ec2-user bash -c "ln -s /usr/share/ansible/openshift-ansible"
 
 cd /root
 curl "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o "awscli-bundle.zip"
 unzip awscli-bundle.zip
-sudo ./awscli-bundle/install -i /usr/local/aws -b /usr/local/bin/aws
+./awscli-bundle/install -i /usr/local/aws -b /usr/local/bin/aws
 
-sudo reboot
+reboot
 
--- helper_scripts/sysprep-openshift.sh
+++ helper_scripts/sysprep-openshift.sh
@@ -2,43 +2,45 @@
 
 exec >/var/log/cloud-init-output.log 2>&1
 
+DEVICE="/dev/$(sudo lsblk | grep -w disk | sort | tail -1 | awk '{print $$1}')"
+
 HN=$(curl http://169.254.169.254/latest/meta-data/hostname)
-sudo hostnamectl set-hostname $${HN}.${ec2domain}
+hostnamectl set-hostname $${HN}.${ec2domain}
 
-sudo rpm -e rh-amazon-rhui-client
-sudo yum clean all
-sudo rm -rf /var/cache/yum
+rpm -e rh-amazon-rhui-client
+yum clean all
+rm -rf /var/cache/yum
 
-sudo subscription-manager register --activationkey='${rhak}' --org='${rhorg}' --force
-sudo subscription-manager status
-sudo subscription-manager repos --disable="*"
-sudo subscription-manager repos \
+subscription-manager register --activationkey='${rhak}' --org='${rhorg}' --force
+subscription-manager status
+subscription-manager repos --disable="*"
+subscription-manager repos \
     --enable="rhel-7-server-rpms" \
     --enable="rhel-7-server-extras-rpms" \
     --enable="rhel-7-fast-datapath-rpms" \
     --enable="rhel-7-server-ose-${ocp_version}-rpms" \
     --enable="rhel-7-server-ansible-${ansible_version}-rpms" 
 
-sudo yum update -y
-sudo yum install -y wget git net-tools bind-utils yum-utils iptables-services bridge-utils bash-completion kexec-tools sos psacct
+yum update -y
+yum install -y wget git net-tools bind-utils yum-utils iptables-services bridge-utils bash-completion kexec-tools sos psacct
 
-sudo yum install -y docker NetworkManager
+yum install -y docker NetworkManager
 
-sudo systemctl stop docker
-sudo systemctl enable docker
-sudo rm -rf /var/lib/docker
-sudo mkdir /var/lib/docker
-sudo wipefs -a /dev/xvdf
-sudo bash -c 'cat <<EOF > /etc/sysconfig/docker-storage-setup
+systemctl stop docker
+systemctl enable docker
+rm -rf /var/lib/docker
+mkdir /var/lib/docker
+wipefs -a $${DEVICE}
+cat >/etc/sysconfig/docker-storage-setup <<EOF
 STORAGE_DRIVER=overlay2
-DEVS=/dev/xvdf
+DEVS=$${DEVICE}
 VG=dockervg
 CONTAINER_ROOT_LV_NAME=dockerlv
 CONTAINER_ROOT_LV_SIZE=100%FREE
 CONTAINER_ROOT_LV_MOUNT_PATH=/var/lib/docker
-EOF'
-sudo docker-storage-setup
-sudo systemctl restart docker
+EOF
+docker-storage-setup
+systemctl restart docker
 
-sudo reboot
+reboot
 
